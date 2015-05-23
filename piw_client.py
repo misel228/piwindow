f6ba2188ef5a0d98736fb7469f7ff1e1271a804d
Your Name <github@misel.net>
2015-05-23 22:21:00 +0200

    first pi3d code

--- piw_client.py
+++ piw_client.py
@@ -1,12 +1,35 @@
+#!/usr/bin/python
+from __future__ import absolute_import, division, print_function, unicode_literals
+import demo
+import pi3d
+
+
+DISPLAY = pi3d.Display.create(x=0, y=0)
+shader = pi3d.Shader("uv_flat")
+CAMERA = pi3d.Camera(is_3d=False)
+sprite = pi3d.ImageSprite("images/testbild_opt.png", shader, w=100.0, h=100.0, z=5.0)
+mykeys = pi3d.Keyboard()
+xloc = 100.0
+yloc = 100.0
+
 #read config
 
 #loop
     #connect to server
     
     #loop
+    while DISPLAY.loop_running():
+    sprite.draw()
+    sprite.position(xloc, yloc, 5.0)
+
         #data on socket?
             #read image/position
             #render image
+    if mykeys.read() == 27:
+        mykeys.close()
+        DISPLAY.destroy()
+        break
 
     #disconnect
 
+  
