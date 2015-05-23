dbbcf3df735f76acf5aa5f03ca3643e72d9a54aa
Stefan Misch <misel@misel.de>
2015-05-23 21:25:59 +0000

    foobar

--- piw_client.py
+++ piw_client.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python
 from __future__ import absolute_import, division, print_function, unicode_literals
-import demo
 import pi3d
 
 
 DISPLAY = pi3d.Display.create(x=0, y=0)
 shader = pi3d.Shader("uv_flat")
 CAMERA = pi3d.Camera(is_3d=False)
-sprite = pi3d.ImageSprite("images/testbild_opt.png", shader, w=100.0, h=100.0, z=5.0)
 mykeys = pi3d.Keyboard()
-xloc = 100.0
-yloc = 100.0
+
 
 #read config
 
+
+
+xloc = 100.0
+yloc = 100.0
+xsize = 1280
+ysize = 800
+zindex = 5
+image_file = "images/testbild_opt.png"
+sprite = pi3d.ImageSprite(image_file, shader, w=xsize, h=ysize, z=zindex)
+
+
 #loop
-    #connect to server
-    
-    #loop
-    while DISPLAY.loop_running():
+#connect to server
+
+#loop
+while DISPLAY.loop_running():
+    #data on socket?
+	#read image(size)/position
+	#sprite = pi3d.ImageSprite(image_file, shader, w=xsize, h=ysize, z=zindex)
+	#sprite.position(xloc, yloc, zindex)
+	#render image
     sprite.draw()
-    sprite.position(xloc, yloc, 5.0)
-
-        #data on socket?
-            #read image/position
-            #render image
     if mykeys.read() == 27:
-        mykeys.close()
-        DISPLAY.destroy()
-        break
+	mykeys.close()
+	DISPLAY.destroy()
+	break
+	#disconnect
 
-    #disconnect
-
-  
