615ef50105676085e77120a8e1b7fd577cd856e2
Stefan Misch <misel@misel.de>
2015-05-24 10:08:52 +0000

    first code for network code

--- piw_client.py
+++ piw_client.py
@@ -2,12 +2,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import pi3d
 import ConfigParser
-
-DISPLAY = pi3d.Display.create(x=0, y=0)
-shader = pi3d.Shader("uv_flat")
-CAMERA = pi3d.Camera(is_3d=False)
-mykeys = pi3d.Keyboard()
-
+import socket
+import sys
 
 #read config
 Config = ConfigParser.ConfigParser()
@@ -15,29 +11,45 @@ Config.read("config.ini")
 
 name   = Config.get("client",'name')
 server = Config.get("client",'server')
-port   = Config.get("client",'port')
+port   = int(Config.get("client",'port'))
 
 
-xloc = Config.get("client",'x_offset')
-yloc = Config.get("client",'y_offset')
+xloc = int(Config.get("client",'x_offset'))
+yloc = int(Config.get("client",'y_offset'))
 
 xsize = 1280
 ysize = 800
 zindex = 5
 image_file = "images/testbild_opt.png"
 
-sprite = pi3d.ImageSprite(image_file, shader, w=xsize, h=ysize, z=zindex)
 
 
 #loop
 #connect to server
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((server,port))
+foo = s.recv(3)
+print("%s" % foo.decode('ascii'))
+s.close
+sys.exit(0)
+
+
+
+DISPLAY = pi3d.Display.create(x=0, y=0)
+shader = pi3d.Shader("uv_flat")
+CAMERA = pi3d.Camera(is_3d=False)
+mykeys = pi3d.Keyboard()
+sprite = pi3d.ImageSprite(image_file, shader, w=xsize, h=ysize, z=zindex)
+
+
+
 
 #loop
 while DISPLAY.loop_running():
     #data on socket?
 	#read image(size)/position
 	#sprite = pi3d.ImageSprite(image_file, shader, w=xsize, h=ysize, z=zindex)
-	#sprite.position(xloc, yloc, zindex)
+    sprite.position(xloc, yloc, zindex)
 	#render image
     sprite.draw()
     if mykeys.read() == 27:
