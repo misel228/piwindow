35f61ef12cb284cb6c92aa93ec3cf9afad72b4b7
Stefan Misch <misel@misel.de>
2015-05-25 22:19:57 +0000

    image position calculation

--- piw_nonet.py
+++ piw_nonet.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import pi3d
 import ConfigParser
 from PIL import Image
+import sys
 
 #read config
 Config = ConfigParser.ConfigParser()
@@ -10,6 +11,11 @@ Config.read("config.ini")
 
 xloc  = int(Config.get("client",'x_offset'))
 yloc  = int(Config.get("client",'y_offset'))
+
+
+x_virtual = int(Config.get("client",'x_virtual'))
+y_virtual = int(Config.get("client",'y_virtual'))
+
 ifile = Config.get("client","default_image")
 im = Image.open(ifile)
 xsize,ysize = im.size
@@ -17,6 +23,16 @@ zindex = 5
 
 
 DISPLAY = pi3d.Display.create(x=0, y=0)
+DISPLAY.set_background(0,0,0,0) #black
+
+xloc = xloc + (x_virtual - DISPLAY.width) / 2
+yloc = yloc + (y_virtual - DISPLAY.height) / 2
+
+
+##print("foo %d " % DISPLAY.width)
+#sys.exit
+
+
 shader = pi3d.Shader("uv_flat")
 CAMERA = pi3d.Camera(is_3d=False)
 mykeys = pi3d.Keyboard()
