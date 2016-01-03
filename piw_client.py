13a37a7cd1f20a89685c889774eea4df3d4f1d2d
Your Name <github@misel.net>
2016-01-03 11:04:33 +0100

    first net + graph version

--- piw_client.py
+++ piw_client.py
@@ -2,59 +2,99 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import pi3d
 import ConfigParser
-import socket
+from PIL import Image
 import sys
+import socket
+
+def coords_str2int(coords):
+	if len(coords) != 22:
+		return False
+	x = coords[0]
+	if x != 'X':
+		return False
+	y = coords[11]
+	if y != 'Y':
+		return false
+	x_value = coords[1:11]
+	y_value = coords[12:22]
+	int_coords = (int(x_value), int(y_value))
+	return int_coords
+
 
 #read config
 Config = ConfigParser.ConfigParser()
 Config.read("config.ini")
 
-name   = Config.get("client",'name')
-server = Config.get("client",'server')
-port   = int(Config.get("client",'port'))
+address = Config.get("client","address")
+port = int(Config.get("client","port"))
+
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	print("Socket created")
+except socket.error, msg:
+	print("Failed to create socket. Error Code : " + str(msg[0]) + " Message " + msg[1])
+	sys.exit()
+
+# Bind socket to local host and port
+try:
+	s.bind((address, port))
+	s.setblocking(0)
+except socket.error , msg:
+	print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+	sys.exit()
+	
+print('Socket bind complete')
 
 
-xloc = int(Config.get("client",'x_offset'))
-yloc = int(Config.get("client",'y_offset'))
 
-xsize = 1280
-ysize = 800
+xloc  = int(Config.get("client",'x_offset'))
+yloc  = int(Config.get("client",'y_offset'))
+
+
+x_virtual = int(Config.get("client",'x_virtual'))
+y_virtual = int(Config.get("client",'y_virtual'))
+
+ifile = Config.get("client","default_image")
+try:
+	im = Image.open(ifile)
+except:
+	print("Could not open file "+ifile)
+	sys.exit()
+
+
+xsize,ysize = im.size
 zindex = 5
-image_file = "images/testbild_opt.png"
-
-
-
-#loop
-#connect to server
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((server,port))
-foo = s.recv(3)
-print("%s" % foo.decode('ascii'))
-s.close
-sys.exit(0)
-
-
 
 DISPLAY = pi3d.Display.create(x=0, y=0)
+DISPLAY.set_background(0,0,0,0) #black
+
+xloc = xloc + (x_virtual - DISPLAY.width) / 2
+yloc = yloc - (y_virtual - DISPLAY.height) / 2
+
+
+
 shader = pi3d.Shader("uv_flat")
 CAMERA = pi3d.Camera(is_3d=False)
 mykeys = pi3d.Keyboard()
-sprite = pi3d.ImageSprite(image_file, shader, w=xsize, h=ysize, z=zindex)
+sprite = pi3d.ImageSprite(ifile, shader, w=xsize, h=ysize, z=zindex)
 
-
-
-
-#loop
 while DISPLAY.loop_running():
-    #data on socket?
-	#read image(size)/position
-	#sprite = pi3d.ImageSprite(image_file, shader, w=xsize, h=ysize, z=zindex)
-    sprite.position(xloc, yloc, zindex)
-	#render image
-    sprite.draw()
-    if mykeys.read() == 27:
-	mykeys.close()
-	DISPLAY.destroy()
-	break
-	#disconnect
+	try:
+		# receive data from client (data, addr)
+		d = s.recvfrom(1024)
+		data = d[0]
+		addr = d[1]
+		if not data: 
+			continue
+		coords = coords_str2int(data.strip())
+	except:
+		f = 'bar' # NOOP
 
+	sprite.position(xloc+coords[0], yloc+coords[1], zindex)
+	sprite.draw()
+	if mykeys.read() == 27:
+		mykeys.close()
+		DISPLAY.destroy()
+		break
+
+s.close()
